320ed4970d0a4e506d3b5a2b2f1a93b74b6f3c82
oogishigen <77260650+oogishigen@users.noreply.github.com>
2022-05-22 18:24:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
+const day = document.getElementById("day");
 const hour = document.getElementById("hour");
 const min = document.getElementById("min");
 const sec = document.getElementById("sec");
 
 function countdown(){
 　　const now = new Date(); //現在時刻を取得
-　　const deadline = new Date(2022, (9-1), 25, 20, 30, 00); //期限の時刻を取得　//monthは0スタートなので1を引く
+　　const deadline = new Date(2022, (6-1), 24, 13, 15, 00); //期限の時刻を取得　//monthは0スタートなので1を引く
 　　const diff = deadline.getTime() - now.getTime(); //時間の差を取得（ミリ秒）
 
 　　//ミリ秒から単位を修正
-　　const calcHour = Math.floor(diff / 1000 / 60 / 60);
-　　const calcMin = Math.floor(diff / 1000 / 60) % 60;
-　　const calcSec = Math.floor(diff / 1000) % 60;
+　　const calcDay = Math.floor(diff / 1000 / 60 / 60 / 24) - 31;
+　　const calcHour = Math.floor(diff % (24 * 60 * 60 * 1000 )/ 1000 / 60 / 60);
+　　const calcMin = Math.floor(diff  % (24 * 60 * 60 * 1000) / 1000 / 60) % 60;
+　　const calcSec = Math.floor(diff  % (24 * 60 * 60 * 1000) / 1000) % 60;
 
 　　//取得した時間を表示（2桁表示）
+　　day.innerHTML = calcDay < 10? '0' + calcDay : calcDay;
 　　hour.innerHTML = calcHour < 10 ? '0' + calcHour : calcHour;
 　　min.innerHTML = calcMin < 10 ? '0' + calcMin : calcMin;
 　　sec.innerHTML = calcSec < 10 ? '0' + calcSec : calcSec;
